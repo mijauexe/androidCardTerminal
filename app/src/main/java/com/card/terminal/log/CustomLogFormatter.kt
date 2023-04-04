0864694e9cb7f2ce14c73e6d307471bc08b3076b
mijauexe <sven.nekic@hotmail.com>
2023-04-04 19:03:58 +0200

    add custom log formatter

---
+++ app/src/main/java/com/card/terminal/log/CustomLogFormatter.kt
@@ -0,0 +1,11 @@
+package com.card.terminal.log
+
+import fr.bipi.tressence.common.formatter.Formatter
+import fr.bipi.tressence.common.time.TimeStamper
+
+class CustomLogFormatter : Formatter {
+    override fun format(priority: Int, tag: String?, message: String): String {
+        val timestamper = TimeStamper("MM-dd HH:mm:ss")
+        return timestamper.getCurrentTimeStamp(System.currentTimeMillis()) + " | " + message + "\n"
+    }
+}
