4b8789eace531e35a8f0c511ab1736539be24f21
mijauexe <sven.nekic@hotmail.com>
2023-05-03 11:14:23 +0200

    improve logging for uncaught exceptions

--- app/src/main/java/com/card/terminal/UEHandler.kt
+++ app/src/main/java/com/card/terminal/UEHandler.kt
@@ -25,12 +25,18 @@ class UEHandler(context: Context, restartActivityClass: Class<*>) :
             PendingIntent.getActivity(mContext, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val alarmManager = mContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager[AlarmManager.RTC, System.currentTimeMillis() + 2000] = pendingIntent
-        println(thread.id)
-        println(thread.name)
-        println(thread.stackTrace)
-        println(ex.stackTrace)
-        println(ex.message)
-        Timber.d("UncaughtException %s | %s | %s", ex.cause, ex.stackTraceToString(), ex.message)
+//        println(thread.id)
+//        println(thread.name)
+//        println(thread.stackTrace)
+//        println(ex.stackTrace)
+//        println(ex.message)
+        Timber.d("Msg: UncaughtException %s | %s | %s | %s | %s | %s",
+            thread.id.toString(),
+            thread.name,
+            thread.stackTrace.toString(),
+            ex.cause,
+            ex.stackTraceToString(),
+            ex.message)
         System.exit(2)
     }
 }
